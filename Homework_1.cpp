7984245abfc0591d8cb1e672503177318817491f
jashp007 <104731665+jashp007@users.noreply.github.com>
2023-09-06 16:56:38 -0500

    THis is final for hw1

--- Homework_1.cpp
+++ Homework_1.cpp
@@ -10,15 +10,19 @@ int main()
 {
 ifstream inputFile;
 string filename;
-getline(cin,filename);
+getline(cin,filename); // getting the line by taking filename input
 string line;
-inputFile.open(filename);
+inputFile.open(filename); // opening the file
+// if will be executed if and only if the file opens otherwise it will go to else condition where it will print the error message
 if(inputFile.is_open()){
 	while (getline(inputFile,line))
 	{
 		cout << line << endl;
 	}
 	
+}
+else{
+	cout << "Error: cannot open file" << endl;
 }
 
 
